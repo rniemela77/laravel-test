cd2a770e5ae1ea502121a2705e44d40b43f57f37
Robert Niemela <rvniemela@hotmail.com>
2022-06-29 17:17:59 -0700

    import html file from slug

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
+
+Route::get('posts/{post}', function($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if (! file_exists($path)) {
+        return redirect('/');
+    }
+
+    // Cache the page in memory for 5 seconds
+    $post = cache()->remember("posts.{$slug}", 5, function() use ($path) {
+        var_dump('file_get_contents');
+        return file_get_contents($path);
+    });
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+');
