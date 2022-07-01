4fbf34553497ac10c91054946dfac522871d55eb
Robert Niemela <rvniemela@hotmail.com>
2022-07-01 08:58:37 -0700

    turn the Post model into an eloquent style migration and model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,73 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public $title;
-    public $excerpt;
-    public $date;
-    public $body;
-    public $slug;
-
-
-    public function __construct($title, $excerpt, $date, $body, $slug)
-    {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-
-    public static function all()
-    {
-        // Cache all posts forever
-        return cache()->rememberForever('posts.all', function () {
-            // Collect all the files in the /posts/ directory
-            return collect(File::files(resource_path("posts")))
-                ->map(function ($file) {
-                    // Loop through the collection and return each document, parsed
-                    return YamlFrontMatter::parseFile($file);
-                })
-                ->map(function ($document) {
-                    // Convert each document into a Post
-                    return new Post(
-                        $document->title,
-                        $document->excerpt,
-                        $document->date,
-                        $document->body(),
-                        $document->slug
-                    );
-                })
-                ->sortByDesc('date');
-        });
-    }
-
-    public static function find($slug)
-    {
-        // Grab all the blog posts
-        $posts = static::all();
-
-        // Return the post with the matching slug
-        return $posts->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug)
-    {
-        // Find the blog post
-        $post = static::find($slug);
-
-        // If no post is found, throw exception
-        if (!$post) {
-            throw new ModelNotFoundException();
-        }
-
-        // Else, return the post
-        return $post;
-    }
+    use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,9 @@ Route::get('/', function () {
         ]);
 });
 
-Route::get('posts/{post}', function ($slug) {
+Route::get('posts/{post}', function ($id) {
     // Find a post by its slug and pass it to a view called "post"
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => Post::findOrFail($id)
     ]);
 });
