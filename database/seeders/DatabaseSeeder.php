66e2844f6673b3321e7162f81bb2de8647dc5f40
Robert Niemela <rvniemela@hotmail.com>
2022-07-01 18:56:23 -0700

    manually seed database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Empty out the tables
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        // Create 1 user in the factory
+         $user = User::factory()->create();
+
+         $personal = Category::create([
+             'name' => 'Personal',
+             'slug' => 'personal'
+         ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        $hobby = Category::create([
+            'name' => 'Hobby',
+            'slug' => 'hobby'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'fake post title',
+            'slug' => 'fake_slug',
+            'excerpt' => 'fake excerpt',
+            'body' => 'fake body'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'fake post title 2',
+            'slug' => 'fake_slug_2',
+            'excerpt' => 'fake excerpt222',
+            'body' => 'fake body222'
+        ]);
     }
 }
