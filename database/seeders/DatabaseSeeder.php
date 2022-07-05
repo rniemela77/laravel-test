37717227e8b95030fb4de00a39e45726f0b3a056
Robert Niemela <rvniemela@hotmail.com>
2022-07-05 12:57:50 -0700

    create a factory for users, posts, and categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,40 +21,8 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Post::truncate();
 
-        // Create 1 user in the factory
-         $user = User::factory()->create();
 
-         $personal = Category::create([
-             'name' => 'Personal',
-             'slug' => 'personal'
-         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        $hobby = Category::create([
-            'name' => 'Hobby',
-            'slug' => 'hobby'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'fake post title',
-            'slug' => 'fake_slug',
-            'excerpt' => 'fake excerpt',
-            'body' => 'fake body'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'fake post title 2',
-            'slug' => 'fake_slug_2',
-            'excerpt' => 'fake excerpt222',
-            'body' => 'fake body222'
-        ]);
+        Post::factory()->create();
     }
 }
