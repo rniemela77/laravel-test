05224e741ad81d1d43d17c2899eb0c563fbf8359
Robert Niemela <rvniemela@hotmail.com>
2022-07-06 09:22:33 -0700

    use username in the URI when filtering posts by author

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
     <h1>{{$post->title}}</h1>
 
     <p>
-        By <a href="/">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
+        By <a href="/">{{$post->author->username}}</a> in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
     </p>
 
     <p>{!!$post->body!!}</p>
--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/categories/{category:slug}', function (Category $category) {
     ]);
 });
 
-Route::get('/authors/{author}', function (User $author) {
+Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts
     ]);
