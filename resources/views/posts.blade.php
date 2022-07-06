95decf2e8708a7f3a9ceb08b59e508ac6c37d1a5
Robert Niemela <rvniemela@hotmail.com>
2022-07-06 16:00:53 -0700

    make featured post reactive

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,18 +2,18 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card/>
+        <x-post-featured-card :post="$posts[0]"/>
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-            <x-post-card/>
-        </div>
+{{--        <div class="lg:grid lg:grid-cols-2">--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--        </div>--}}
 
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card/>
-            <x-post-card/>
-            <x-post-card/>
-        </div>
+{{--        <div class="lg:grid lg:grid-cols-3">--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--        </div>--}}
     </main>
 
     {{--    @foreach ($posts as $post)--}}
