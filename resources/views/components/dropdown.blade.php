8700f49944324f87275695e138bb8af678d59332
Robert Niemela <rvniemela@hotmail.com>
2022-07-07 12:39:43 -0700

    create dropdown components

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,14 @@
+@props(['trigger'])
+
+<div x-data="{ show: false }">
+    {{-- Trigger --}}
+    <div @click="show = !show" @click.away="show = false">
+        {{ $trigger }}
+    </div>
+
+
+    {{--  Dropdown Links  --}}
+    <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display: none">
+        {{ $slot }}
+    </div>
+</div>
