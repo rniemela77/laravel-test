19bcb91a46406c61aa5882405ac18d135d331828
Robert Niemela <rvniemela@hotmail.com>
2022-07-07 14:11:18 -0700

    do not escape html tags in post body

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -42,7 +42,7 @@
                 </h1>
 
                 <div class="space-y-4 lg:text-lg leading-loose">
-                    {{ $post->body }}
+                    {!! $post->body !!}
                 </div>
             </div>
         </article>
