9184bf4dfbac7cc2649329f7e40f26ce50d12108
Robert Niemela <rvniemela@hotmail.com>
2022-07-06 18:48:36 -0700

    show current filtered category in the category dropdown

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -21,7 +21,7 @@
                     @click.away="show = false"
                     class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
                 >
-                    Categories
+                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                          height="22" viewBox="0 0 22 22">
                         <g fill="none" fill-rule="evenodd">
--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::get('posts/{post}', function (Post $post) { // Post::where('slug', $post)
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
         'categories' => Category::all()
     ]);
 });
