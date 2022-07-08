7fd886beae66c2a738e343ad4956b73122f22bc2
Robert Niemela <rvniemela@hotmail.com>
2022-07-08 16:25:16 -0700

    allow pagination on current filter settings

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
         // Render the 'posts' view, and pass in the collection of Posts
         return view('posts.index',
             [
-                'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(3)
+                'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(3)->withQueryString()
             ]);
     }
 
