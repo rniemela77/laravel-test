76cddf4df8b6d046484493f741dcb6776b4550dd
Robert Niemela <rvniemela@hotmail.com>
2022-07-08 15:54:29 -0700

    fix eloquent query OR/AND logic

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,9 +17,11 @@ class Post extends Model
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? false, function ($query, $search) {
-            $query
-                ->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search . '%');
+            $query->where(function ($query) use ($search) {
+                $query
+                    ->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('body', 'like', '%' . $search . '%');
+            });
         });
 
         $query->when($filters['category'] ?? false, function ($query, $category) {
