27e36014bc9527126bc912a5cd7fdb55c4922d69
Robert Niemela <rvniemela@hotmail.com>
2022-07-12 10:26:04 -0700

    if logged-in user attempts to go to /register, redirect to /

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post}', [PostController::class, 'show']);
 
-Route::get('/register', [RegisterController::class, 'create']);
-Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
