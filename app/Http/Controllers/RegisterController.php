fde7d61a698341347f82d86a0f1f66ed57a91f77
Robert Niemela <rvniemela@hotmail.com>
2022-07-12 10:18:47 -0700

    log user in on successful register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,7 +23,10 @@ class RegisterController extends Controller
             'password' => ['required', 'min:3']
         ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        // Log the user in
+        auth()->login($user);
 
         return redirect('/')->with('success', 'Your account has been created.');
     }
