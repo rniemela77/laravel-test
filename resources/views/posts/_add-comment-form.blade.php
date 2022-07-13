8dc2558383a29098ae79972f5e9c01e4676e6450
Robert Niemela <rvniemela@hotmail.com>
2022-07-13 11:02:21 -0700

    create component for submit button

--- resources/views/posts/_add-comment-form.blade.php
+++ resources/views/posts/_add-comment-form.blade.php
@@ -25,10 +25,7 @@
             </div>
 
             <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                <button type="submit"
-                        class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
-                    Post
-                </button>
+                <x-submit-button>Post</x-submit-button>
             </div>
         </form>
     </x-panel>
