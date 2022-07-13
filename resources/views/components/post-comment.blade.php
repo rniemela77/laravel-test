a8fe35f1f9acd3ed41e0ab2076b0784846c63218
Robert Niemela <rvniemela@hotmail.com>
2022-07-13 10:36:33 -0700

    format comment date

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -9,7 +9,7 @@
             <header class="mb-4">
                 <h3 class="font-bold">{{ $comment->author->username }}</h3>
                 <p class="text-xs">
-                    <time>{{ $comment->created_at }}</time>
+                    <time>{{ $comment->created_at->format('F j, Y, g:i a') }}</time>
                 </p>
             </header>
 
